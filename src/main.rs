ed2e14661530331e84bcb0bfba5a2ce445d73e1d
Akiomi Kamakura <akiomik@gmail.com>
2021-11-08 23:12:21 +0900

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 
 use askama::Template;
+use clap::{App, Arg};
 use scraper::{Html, Selector};
 
 struct Item<'a> {
@@ -17,12 +18,34 @@ struct SafariTemplate<'a> {
 }
 
 fn main() {
-    let input = match fs::read_to_string("ril_export.html") {
+    let matches = App::new("reading-list-converter")
+        .version("0.1.0")
+        .author("Akiomi Kamakura <akiomik@gmail.com>")
+        .args(&[
+            Arg::with_name("input")
+                .long("input")
+                .value_name("FILE")
+                .takes_value(true)
+                .required(true),
+            Arg::with_name("output")
+                .long("output")
+                .value_name("FILE")
+                .takes_value(true)
+                .required(true),
+        ])
+        .get_matches();
+
+    let input_file = matches.value_of("input").unwrap();
+    let input = match fs::read_to_string(input_file) {
         Ok(input) => input,
-        Err(e) => panic!("failed to read file: {}", e),
+        Err(e) => panic!("failed to read '{}': {}", input_file, e),
     };
 
-    let mut file = BufWriter::new(File::create("pocket-to-safari.html").unwrap());
+    let output_file = matches.value_of("output").unwrap();
+    let output = match File::create(output_file) {
+        Ok(output) => output,
+        Err(e) => panic!("failed to create '{}': {}", output_file, e),
+    };
 
     let document = Html::parse_document(&input);
     let selector = Selector::parse("body > ul > li > a").unwrap();
@@ -38,8 +61,8 @@ fn main() {
     }
 
     let tmpl = SafariTemplate { items };
-    match file.write(tmpl.render().unwrap().as_bytes()) {
-        Ok(_) => println!("done!"),
+    match BufWriter::new(output).write(tmpl.render().unwrap().as_bytes()) {
+        Ok(_) => (),
         Err(e) => panic!("failed to convert reading list: {}", e),
     };
 }
